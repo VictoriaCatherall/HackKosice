88c75aa2d00c8768ad53d3fde3cf067ec5dea83e
Tom Cusack <tom.cusack@hotmail.co.uk>
2020-09-05 11:23:21 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,23 @@
 <php
-echo json_encode({
-  "text": "shut up"
-})
+
+$data = array(
+  "text" => "shut up",
+)
+
+$json_string = json_encode($data)
+
+
+$slack_call = curl_init($slack_webhook_url);
+curl_setopt($slack_call, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($slack_call, CURLOPT_POSTFIELDS, $json_string);
+curl_setopt($slack_call, CURLOPT_CRLF, true);
+curl_setopt($slack_call, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($slack_call, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+    "Content-Length: " . strlen($json_string))
+);
+
+$result = curl_exec($slack_call);
+curl_close($slack_call);
+
 ?>
