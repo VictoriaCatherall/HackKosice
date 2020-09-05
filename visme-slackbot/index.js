e4cbd5f2557baf384f3946d8502b9f386aa8d4a0
George Bakewell <gbakewell7@gmail.com>
2020-09-05 22:46:41 +0100

    Compress errors

--- visme-slackbot/index.js
+++ visme-slackbot/index.js
@@ -35,12 +35,9 @@ slackEvents.on('message', (event) => {
   console.log(`message.im: ${event.text}`);
   faq.ask(event.text, (err, answer) => {
     const channelId = event.channel;
-    if (err) {
-      web.chat.postMessage({ channel: channelId, text: "<https://www.youtube.com/watch?v=dQw4w9WgXcQ|HHAHAHA>" });
-    } else if (answer.answer == 'No good match found in KB.') {
+    if (err || answer.answer == 'No good match found in KB.') {
       let dates = chatbot.toJSDates(chatbot.getDates(event.text));
       if (dates.length == 0) {
-        //see if name else
         web.chat.postMessage({ channel: channelId, text: "<https://www.youtube.com/watch?v=dQw4w9WgXcQ|HHAHAHA>" });
       } else if (dates.length == 1) {
         let day = chatbot.dayBounds(dates[0]);
@@ -60,9 +57,7 @@ slackEvents.on('message', (event) => {
       } else {
         web.chat.postMessage({ channel: channelId, text: "<https://www.youtube.com/watch?v=dQw4w9WgXcQ|HHAHAHA>" });
       }
-      
-    }
-    else {
+    } else {
       web.chat.postMessage({ channel: channelId, text: answer.score + answer.answer });
     }
   });
