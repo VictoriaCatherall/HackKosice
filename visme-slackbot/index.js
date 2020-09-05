3f50d0dd1709e7dca9ffbf1262f3d649a3369bab
George Bakewell <gbakewell7@gmail.com>
2020-09-05 12:44:24 +0100

    Combine two sample files

--- visme-slackbot/index.js
+++ visme-slackbot/index.js
@@ -18,3 +18,21 @@ const conversationId = process.env.CHANNEL_ID;
   // `res` contains information about the posted message
   console.log('Message sent: ', res.ts);
 })();
+
+// https://slack.dev/node-slack-sdk/events-api
+
+const { createEventAdapter } = require('@slack/events-api');
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
+const slackEvents = createEventAdapter(slackSigningSecret);
+const port = process.env.PORT || 3000;
+
+// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+slackEvents.on('message', (event) => {
+  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
+});
+
+(async () => {
+  const server = await slackEvents.start(port);
+  console.log(`Listening for events on ${server.address().port}`);
+})();
+
