16aa076c488b5d7a92929bb2738d5614188955bb
VictoriaCatherall <catherall.victoria@gmail.com>
2020-09-05 21:52:06 +0100

    ADD linking dates

--- visme-slackbot/index.js
+++ visme-slackbot/index.js
@@ -4,10 +4,12 @@
 
 // https://slack.dev/node-slack-sdk/events-api
 
+
 const { createEventAdapter } = require('@slack/events-api');
 const { WebClient } = require('@slack/web-api');
 
 const chatbot = require('./chatbot');
+const calendar = require('./google-calendar')
 const faq = require('./faq');
 
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
@@ -32,9 +34,24 @@ slackEvents.on('message', (event) => {
   console.log(`message.im: ${event.text}`);
   faq.ask(event.text, (err, answer) => {
     const channelId = event.channel;
-    if (err || answer.answer == 'No good match found in KB.') {
+    if (err) {
       web.chat.postMessage({ channel: channelId, text: "<https://www.youtube.com/watch?v=dQw4w9WgXcQ|HHAHAHA>" });
-    } else {
+    } else if (answer.answer == 'No good match found in KB.') {
+      let dates = chatbot.toJSDates(chatbot.getDates(event.text));
+      if (dates.length == 0) {
+        //see if name else
+        web.chat.postMessage({ channel: channelId, text: "<https://www.youtube.com/watch?v=dQw4w9WgXcQ|HHAHAHA>" });
+      } else if (dates.length == 1) {
+        let day = chatbot.dayBounds(dates[0]);
+        calendar.getEvents(day[0], day[1]);
+      } else if (dates.length == 2) {
+        calendar.getEvents(dates[0], dates[1]);
+      } else {
+        web.chat.postMessage({ channel: channelId, text: "<https://www.youtube.com/watch?v=dQw4w9WgXcQ|HHAHAHA>" });
+      }
+      
+    }
+    else {
       web.chat.postMessage({ channel: channelId, text: answer.score + answer.answer });
     }
   });
