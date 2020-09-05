22d67fea70e260d5ebb3148bbe920216216e439b
VictoriaCatherall <50451821+VictoriaCatherall@users.noreply.github.com>
2020-09-05 16:13:05 +0100

    addd verb and date

--- visme-slackbot/chatbot/index.js
+++ visme-slackbot/chatbot/index.js
@@ -5,7 +5,18 @@ function getNouns(text) {
   return doc.nouns().json();
 }
 
+function getVerbs(text) {
+  const doc = nlp(text);
+  return doc.verbs().json();
+}
+
+function getDate(text) {
+  const doc = nlp(text);
+  return doc.dates().json();
+}
+
 module.exports = {
   getNouns,
+  getVerbs,
+  getDate
 };
-
