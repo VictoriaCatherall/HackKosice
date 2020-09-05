89825313120e493c446c4d24dea8483371ab800a
George Bakewell <gbakewell7@gmail.com>
2020-09-05 12:54:21 +0100

    Move calendar code to within node project

--- visme-slackbot/google-calendar/index.js
+++ visme-slackbot/google-calendar/index.js
@@ -22,7 +22,7 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback){
+function authorize(credentials, callback) {
   const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
@@ -91,4 +91,4 @@ function listEvents(auth) {
       console.log('No upcoming events found.');
     }
   });
-} 
+}
