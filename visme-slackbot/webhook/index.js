e6dfe622a0fd932a419f0213d0e92c5b04733d4b
rjb255 <62777572+rjb255@users.noreply.github.com>
2020-09-06 02:23:08 +0100

    Update index.js
    googleAssist

--- visme-slackbot/webhook/index.js
+++ visme-slackbot/webhook/index.js
@@ -1,5 +1,22 @@
 module.exports = (req, res, next) => {
   console.log('incoming webhook');
   console.log(req.body);
-  next(); // must call this after your stuff finishes
+  var clump = JSON.parse(req.body);
+  var querrr = clump["intent"]["query"];
+  dummy(querrr, function (ans) {
+    res.status = 200;
+    ////////////
+    var msg = {
+      prompt: {
+        override: true,
+        firstSimple: {
+          speech: ans,
+          text: ans
+        }
+      }
+    }
+    res.send(msg);
+
+    next();
+  });
 };
